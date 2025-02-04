bc3598d5d7ff52cfc2d752672b37924feda77f78
Flora-Tsakeu <flora.tsakeu@facsciences-uy1.cm>
2025-02-04 18:16:48 +0100

    Ajout de la gestion des produits ,de la gestion des commandes et de la mise en oeuvre de la notification des utilisateurs par email

--- app/schemas.py
+++ app/schemas.py
@@ -22,12 +22,35 @@ class ProductCreate(BaseModel):
     stock: int
 
 
+class ProductResponse(BaseModel):
+    id: int
+    seller: int  # ID du vendeur
+    name: str
+    price: float
+    city: str
+    stock: int
+
+    class Config:
+        orm_mode = True
+
+
 class OrderCreate(BaseModel):
     user_id: int
     product_id: int
     quantity: int
     total_price: float
 
+
+class OrderResponse(BaseModel):
+    id: int
+    product_id: int
+    user_id: int
+    quantity: int
+    status: str
+    total_price: float
+
+    class Config:
+        orm_mode = True
 
 class PostCreate(BaseModel):
     product_id: int
