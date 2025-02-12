320d2d086527efd0dbc5dd21b34504bd45fb7ee8
macdallas <roylexstephane@gmail.com>
2025-02-12 16:35:19 +0100

    add librairie requests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import init_db, close_db
-from app.routes import orders, posts, products, training_materials, users, protected, notifications, disease_detection
+from app.routes import orders, posts, products, training_materials, users, protected, notifications , disease_detection
 
 app = FastAPI()
 
